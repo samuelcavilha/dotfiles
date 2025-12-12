28759b0167f945be6ad22e2df34749fd768057a6
Samuel Cavilha <samuelcavilha@gmail.com>
2025-12-12 07:50:09 -0300

    some basic auto indeneting

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -3,47 +3,30 @@ return {
     "saghen/blink.cmp",
     dependencies = "rafamadriz/friendly-snippets",
 
-<<<<<<< HEAD
-  version = '1.*',
-  opts = {
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-      per_filetype = {
-        sql = { 'snippets', 'dadbod', 'buffer' },
-      },
-      -- add vim-dadbod-completion to your completion providers
-      providers = {
-        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-      },
-    },
-    keymap = {
-      preset = "default",
-      ["<CR>"] = { "accept", "fallback" },
-      ["<C><leader>"] = { "show" },
-    },
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-
-    completion = { documentation = { auto_show = true } },
-
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-=======
-    version = "v0.*",
-
+    version = "1.*",
     opts = {
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+        },
+        -- add vim-dadbod-completion to your completion providers
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
       keymap = {
         preset = "default",
         ["<CR>"] = { "accept", "fallback" },
         ["<C><leader>"] = { "show" },
       },
       appearance = {
-        use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
       },
 
-      signature = { enabled = true },
+      completion = { documentation = { auto_show = true } },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
->>>>>>> 51f043d (changed some omarchy configs with the new update. Tomux: projects/go)
   },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-				ensure_installed = { "lua", "javascript", "html" },
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-				auto_install = true,
-			})
-		end,
-	},
+      configs.setup({
+        ensure_installed = { "lua", "javascript", "html", "python" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        auto_install = true,
+      })
+    end,
+  },
 }
